5a897a64d91672d33af28c0f427fe6992877509c
Daniel Lima <eunotomocaf@gmail.com>
2017-06-02 14:49:48 -0300

    Criando easyaction class

---
+++ EasyAllertViewFrameWork/EasyAllertViewFrameWork/EasyAction.swift
@@ -0,0 +1,17 @@
+//
+//  EasyAction.swift
+//  EasyAllertViewFrameWork
+//
+//  Created by Daniel Lima on 02/06/17.
+//  Copyright © 2017 isaiasmelof. All rights reserved.
+//
+
+import UIKit
+
+class EasyAction: UIAlertAction {
+
+    
+    
+    
+    
+}
